d00941dbf8cf640d06bd0bb299481b1360791a6b
loccoc1 <133509083+loccoc1@users.noreply.github.com>
2023-05-19 23:02:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    titleWeb: "Gửi cho chị NgaNgok",
+    introTitle: 'Dương Thị Quỳnh Nga',
+    introDesc: `Đây là mụt bài khảo sát Top 1,0 Mỹ nhân vũ trụ nên là hãy trả lời trung thực nhé`,
+    btnIntro: '^^Oke HiHi^^',
+    title: 'Ngoài bản thân mình ra chị còn thấy ai xinh đẹp hơn không ( Thủy Tiên, Ngọc Trinh, Triệu Lộ 4, Ronaldo,... 🥰',
+    desc: 'Phải trả lời trung thựt 1000% nhaaa',
+    btnYes: 'Có cl tao là nhất, bọn kia tuổi <33',
+    btnNo: 'Vẩn có́ ngừi xinh hơn tui mà :3',
+    question: 'Được ruồi tôi đả thấy chị cười từ bên này 🤡🤡, tại sao zậy :3',
+    btnReply: 'Gửi cho emmm :3',
+    reply: 'Bởi vì tuôi là một nữ tú băng thanh ngọc khiết, mắt biếc ngọc ngà, thật thà ( hơi hơi ) đáng yo🤡🤡',
+    mess: 'Đc ruồi em biếc mà 😉. Chị thấy zui là tút rùi 💃💃',
+    messDesc: 'Hum nai có chuyện zì hay 10 năm trước có biến sao kể e nghe nha, em nghe🤠',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://github.com/zukahai/Confess' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
